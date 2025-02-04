80f1376f55e2d236bacb3ba1bc017d92aabb7488
techb2bnew <techbase2brand@gmail.com>
2025-02-04 10:31:22 +0530

    build issu

--- src/app/admin/listing/page.tsx
+++ src/app/admin/listing/page.tsx
@@ -1,18 +1,18 @@
 "use client";
 import AdminListing from './admin'
 import React, { useState } from 'react';
-import Pagination from '../../component/pagination';
+// import Pagination from '../../component/pagination';
 import Sidebar from '../../component/sidebar/page';
 export default function Technicians() {
-    const [currentPage, setCurrentPage] = useState(1);
-    const totalPages = 3; // Sample total pages
+    // const [currentPage, setCurrentPage] = useState(1);
+    // const totalPages = 3; // Sample total pages
   
   return (
     <div className='main-container'>
         <Sidebar />
         <div className="right_section w-[85%] pl-6 pr-8 ml-auto mt-[7rem]">
             <AdminListing />
-            <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} />
+            {/* <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} /> */}
         </div>
     </div>
   );
