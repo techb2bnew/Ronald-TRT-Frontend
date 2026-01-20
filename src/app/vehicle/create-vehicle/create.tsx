307bd470eedc749364bd655f040262509f00d476
saurabh bhatia <s.bhatiaodesk@gmail.com>
2026-01-20 20:51:04 +0530

    changes

--- src/app/vehicle/create-vehicle/create.tsx
+++ src/app/vehicle/create-vehicle/create.tsx
@@ -23,7 +23,7 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import type { Dayjs } from 'dayjs';
-import dayjs from 'dayjs';
+import dayjs from 'dayjs'; 
 
 // import { CKEditor } from '@ckeditor/ckeditor5-react'; 
 // import type { Editor } from '@ckeditor/ckeditor5-core';
